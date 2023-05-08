ccf5611647f64e7b664b06bd77332acf0e593ee8
Jeong4939 <jeongp12@gmail.com>
2023-05-08 17:54:43 +0900

    230508 commit

---
+++ JeongHoon/Baekjoon/Study_problem/May/S2_1927_최소_힙.py
@@ -0,0 +1,14 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+heap = []
+for i in range(n):
+    a = int(input())
+    if not a:
+        if heap: print(heapq.heappop(heap))
+        else: print(0)
+    else:
+        heapq.heappush(heap,a)
