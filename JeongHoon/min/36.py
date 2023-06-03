43278ad162e0233bc2e57a55f2af095a5453695a
jeongp4939 <jeongp12@gmail.com>
2023-06-03 20:45:27 +0900

    230603 problem solving

--- JeongHoon/min/36.py
+++ JeongHoon/min/36.py
@@ -1,35 +1,38 @@
+"""
 # 1 알뜰 기차여행
-#
-# from heapq import heappush,heappop
-# INF = 1e11
-# def dijkstra(st):
-#     q=[]
-#     heappush(q,(0,st))
-#     distance[st]=0
-#
-#     while q:
-#         dist,node = heappop(q)
-#         if distance[node] < dist:
-#             continue
-#         for next in graph[node]:
-#             cost = distance[node] + next[1]
-#             if cost < distance[next[0]]:
-#                 distance[next[0]] = cost
-#                 heappush(q, (cost,next[0]))
-#     return distance[n-1] if distance[n-1]<INF else 'impossible'
-#
-#
-# n,v = map(int,input().split())
-# graph = [[] for _ in range(n)]
-# for _ in range(v):
-#     a,b,c = map(int,input().split())
-#     graph[a].append((b,c))
-# distance = [INF]*n
-#
-# print(dijkstra(0))
+
+from heapq import heappush,heappop
+INF = 1e11
+def dijkstra(st):
+    q=[]
+    heappush(q,(0,st))
+    distance[st]=0
+
+    while q:
+        dist,node = heappop(q)
+        if distance[node] < dist:
+            continue
+        for next in graph[node]:
+            cost = distance[node] + next[1]
+            if cost < distance[next[0]]:
+                distance[next[0]] = cost
+                heappush(q, (cost,next[0]))
+    return distance[n-1] if distance[n-1]<INF else 'impossible'
 
 
+n,v = map(int,input().split())
+graph = [[] for _ in range(n)]
+for _ in range(v):
+    a,b,c = map(int,input().split())
+    graph[a].append((b,c))
+distance = [INF]*n
+
+print(dijkstra(0))
+"""
+"""
 # 2 통행료
+
+# 양방향인걸 고려하지 않아서 계속 틀렸음
 
 from heapq import heappush,heappop
 INF = 1e11
@@ -37,7 +40,7 @@ INF = 1e11
 def dijkstra(st,ed):
     q=[]
     heappush(q,(0,st))
-    distance = [INF] * (n + 1)
+    distance = [0]+[INF] * n
     distance[st]=0
 
     while q:
@@ -59,6 +62,7 @@ graph = [[] for _ in range(n+1)]
 for _ in range(m):
     f,t,c = map(int,input().split())
     graph[f].append([t,c])
+    graph[t].append([f,c])
 
 print(dijkstra(a, b))
 
@@ -69,6 +73,6 @@ for _ in range(k):
         for j in range(len(graph[i])):
             graph[i][j][1] += p
     print(dijkstra(a,b))
-
+"""
 
 
