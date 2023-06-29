719ada274e704a41ac5704cbb60546763ca4785e
jeongp4939 <jeongp12@gmail.com>
2023-06-29 16:54:13 +0900

    230629 commit

--- JeongHoon/Baekjoon/Study_problem/June/G4_1261_알고스팟.py
+++ JeongHoon/Baekjoon/Study_problem/June/G4_1261_알고스팟.py
@@ -1,30 +1,33 @@
 import sys
-sys.setrecursionlimit(100000)
+from collections import deque
+
+input = sys.stdin.readline
 
 dy,dx = (1,0,-1,0),(0,1,0,-1)
-def dfs(y,x,cnt=0,depth=0):
-    global Min
-    if cnt>=Min or depth>=m*n:
-        return
-    if y==n-1 and x==m-1:
-        Min = min(Min,cnt)
-        return
-    for i in range(4):
-        ny = y+dy[i]
-        nx = x+dx[i]
-        if 0<=ny<n and 0<=nx<m:
-            if visited[ny][nx]==0:
-                visited[ny][nx] = 1
-                if arr[ny][nx]==0:
-                    dfs(ny,nx,cnt,depth+1)
+def bfs(y,x):
+    visited = [[-1] * m for _ in range(n)]
+    visited[0][0] = 0
+
+    q = deque()
+    q.append((y,x))
+
+    while q:
+        y,x = q.popleft()
+        for i in range(4):
+            ny = y+dy[i]
+            nx = x+dx[i]
+            if 0<=ny<n and 0<=nx<m and visited[ny][nx]==-1:
+                if arr[ny][nx] == 0:
+                    visited[ny][nx]=visited[y][x]
+                    q.appendleft((ny,nx))
                 else:
-                    dfs(ny,nx,cnt+1,depth+1)
-                visited[ny][nx]=0
+                    visited[ny][nx] = visited[y][x]+1
+                    q.append((ny,nx))
+    return visited[n-1][m-1]
+
 
 m,n = map(int,input().split())
-arr = [list(map(int,list(input()))) for _ in range(n)]
-visited = [[0]*m for _ in range(n)]
-visited[0][0]=1
-Min = 10e10
-dfs(0,0)
-print(Min)
+arr = [list(map(int,input().rstrip())) for _ in range(n)]
+
+print(bfs(0,0))
+
