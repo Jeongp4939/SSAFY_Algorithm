368b55e603045196ec362b777cae5495b6ee4da0
jeongp4939 <jeongp12@gmail.com>
2023-06-30 21:24:43 +0900

    230630 commit

--- JeongHoon/Baekjoon/Study_problem/June/G5_1038_감소하는_수.py
+++ JeongHoon/Baekjoon/Study_problem/June/G5_1038_감소하는_수.py
@@ -1,19 +1,16 @@
-def dfs(depth, num=[]):
-
-    if depth==10:
-        return
-    if ''.join(sorted(num, reverse=True)) not in S:
-        S.add(''.join(sorted(num, reverse=True)))
-
-    for i in range(10):
-        if not used[i]:
-            used[i]=1
-            dfs(depth,num+[str(i)])
-            used[i]=0
+from itertools import combinations
 
 N = int(input())
-used = [0]*10
-S = set()
-dfs(0)
-result = sorted(list(S))
-print(result(N))
+
+result = []
+for i in range(1, 11):
+    for j in combinations(range(10), i):  # 조합(중복x)
+        num = ''.join(list(map(str, reversed(list(j)))))  # 0 ~ 9876543210 까지 문자열 생성
+        result.append(int(num)) # 인트형으로 result에 저장
+
+result.sort() # 오름차순으로 정렬
+if N >= len(result):
+    print(-1)
+else:
+    print(result[N])
+
