5c914831d93c260d32eef6f6ea35f85514bb0577
박정훈 <ggongjja0@naver.com>
2023-07-07 08:56:32 +0900

    230707 algoritm python commit

---
+++ JeongHoon/Baekjoon/Study_problem/July/G5_2230_수_고르기.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+lst = []
+for i in range(n):
+    a =int(input())
+    lst.append(a)
+
+lst.sort()
+
+left = right = 0
+result = 2e9
+
+while left<=right and right < n:
+    gap = lst[right] - lst[left]
+    if gap < m:
+        right +=1
+    else:
+        result = min(gap,result)
+        left +=1
+
+print(result)
