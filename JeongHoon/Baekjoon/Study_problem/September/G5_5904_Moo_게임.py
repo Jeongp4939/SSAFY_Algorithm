7a970925dff58f981c7f6b0ccda0e4e6e1816475
박정훈 <jeongp12@gmail.com>
2023-09-08 16:10:35 +0900

    temp

---
+++ JeongHoon/Baekjoon/Study_problem/September/G5_5904_Moo_게임.py
@@ -0,0 +1,13 @@
+# moo 0
+# s = 'moomooomoo' # 0 3 7
+# s = 'moomooomoomoooomoomooomoo' # 0 3 7 10 15 18 22
+# s = 'moomooomoomoooomoomooomoomooooomoomooomoomoooomoomooomoo' # 0 3 7 10 15 18 22 25 31 34 38 41 46 49 53
+
+import time
+
+st = time.time()
+
+
+
+ed = time.time()
+print(st-ed)
