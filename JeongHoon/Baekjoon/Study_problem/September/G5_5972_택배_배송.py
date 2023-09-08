88dc4f39cd3667831e1b6623fa3df143c5e10def
박정훈 <jeongp12@gmail.com>
2023-09-08 09:00:16 +0900

    feat: baekjoon problem solving

--- JeongHoon/Baekjoon/Study_problem/September/G5_5972_택배_배송.py
+++ JeongHoon/Baekjoon/Study_problem/September/G5_5972_택배_배송.py
@@ -1,18 +1,24 @@
-def dfs(s,e,cost=0):
-    global MIN
-    if s==e:
-        MIN = min(MIN,cost)
-        return
-    for ne,nc in graph[s]:
-        if not visited[ne]:
-            visited[ne]=1
-            dfs(e,ne,cost+nc)
-            visited[ne] = 0
+import heapq
+INF = int(2e10)
+def dijkstra(s):
+    q = []
+    heapq.heappush(q,(0,s))
+    distances[s] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+        if distances[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist+i[1]
+
+            if cost<distances[i[0]]:
+                distances[i[0]] = cost
+                heapq.heappush(q,(cost,i[0]))
 
 n,m = map(int,input().split())
-graph = [[]for _ in range(n+1)]
-visited = [0]*(n+1)
-visited[1] = 1
+graph = [[] for _ in range(n+1)]
+distances = [INF]*(n+1)
 MIN = 2e10
 
 for _ in range(m):
@@ -20,6 +26,6 @@ for _ in range(m):
     graph[s].append((e,c))
     graph[e].append((s,c))
 
-dfs(1,n)
+dijkstra(1)
 
-print(MIN)
+print(distances[n])
